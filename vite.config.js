a270f43be09f4e614b725c125a0d883b7d50256c
David Wolter <david@dampfer.net>
2023-05-10 13:41:04 +0200

    typo

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
     // If our .vue files have a style, it will be compiled as a single `.css` file under /dist.
     plugins: [
-        Vue(/*{ style: { filename: 'style.css' } }*/),
+        Vue({ style: { filename: 'style.css' } }),
         visualizer({
             filename: resolve(__dirname, 'stats.html'),
             template: 'treemap', // sunburst|treemap|network
@@ -21,10 +21,10 @@ export default defineConfig({
             // Set the entry point (file that contains our components exported).
             entry: resolve(__dirname, 'src/index.ts'),
             // Name of the library.
-            name: 'jsonforms-vue-vanilla-boplus',
+            name: 'jsonforms-vue-vanilla',
             // We are building for CJS and ESM, use a function to rename automatically files.
             // Example: my-component-library.esm.js
-            fileName: (format) => `${'jsonforms-vue-vanilla-boplus'}.${format}.js`,
+            fileName: (format) => `${'jsonforms-vue-vanilla'}.${format}.js`,
         },
         rollupOptions: {
             // Vue is provided by the parent project, don't compile Vue source-code inside our library.
